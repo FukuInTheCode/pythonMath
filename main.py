2b182a559d5b686774fa8958f4340058b8662fd9
FukuInTheCode <fukulovecoding@gmail.com>
2023-02-18 22:53:04 +0100

    aa

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from src.my_2dProjection import projection_to2d
 import pygame as pyg
-import numpy as np
 from src.objects import Point
 
 def main():
--- src/objects.py
+++ src/objects.py
@@ -17,18 +17,18 @@ class Point:
     ])
 
     def __init__(self, x: int, y: int, z: int) -> None:
-        self.vector = np.matrix([x, y, z]).reshape(3, 1)
+        self._vector = np.matrix([x, y, z]).reshape(3, 1)
+        self.vector = self._vector.copy()
         
     def draw(self, screen: pyg.Surface, scale:int) -> None:
         proj2d = np.dot(self.projection_matrix, self.vector)
         
-        print(proj2d[0][0])
         x = int(proj2d[0][0] * scale) + screen.get_width()/2
         y = int(proj2d[1][0] * scale) + screen.get_height()/2
-        
-        pyg.draw.circle(screen, self.RED, (x, y), scale)
+                
+        pyg.draw.circle(screen, self.RED, (x, y), 5)
         
     def update(self, transform: np.ndarray) -> None:
-        self.vector = np.dot(transform, self.vector)
+        self.vector = np.dot(transform, self._vector)
         
                 
